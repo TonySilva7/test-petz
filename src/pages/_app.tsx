61590f6fa71a40215d8c6fd9f9ee331c85bc98c1
TonySilva7 <tony.deve7oper@gmail.com>
2023-09-19 00:05:04 -0300

    chore: configure styled-components and eslint vs prettier

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+import type { AppProps } from 'next/app';
+import { theme } from '../styles/theme';
+import { GlobalStyles } from '../styles/global-styles';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
